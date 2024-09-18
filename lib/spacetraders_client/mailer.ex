fb38f94ab3e4bdd29f69bb39d9b865f54a1d6df4
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-09-18 16:58:30 -0600

    New Phoenix project
    Created with this command:
    
       mix phx.new spacetraders_client --no-ecto

---
+++ lib/spacetraders_client/mailer.ex
@@ -0,0 +1,3 @@
+defmodule SpacetradersClient.Mailer do
+  use Swoosh.Mailer, otp_app: :spacetraders_client
+end
---
+++ test/spacetraders_client_web/controllers/error_html_test.exs
@@ -0,0 +1,14 @@
+defmodule SpacetradersClientWeb.ErrorHTMLTest do
+  use SpacetradersClientWeb.ConnCase, async: true
+
+  # Bring render_to_string/4 for testing custom views
+  import Phoenix.Template
+
+  test "renders 404.html" do
+    assert render_to_string(SpacetradersClientWeb.ErrorHTML, "404", "html", []) == "Not Found"
+  end
+
+  test "renders 500.html" do
+    assert render_to_string(SpacetradersClientWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+  end
+end
